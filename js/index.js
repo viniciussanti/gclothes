0c371321d64762dd6b1e19d85fa5ca7cd3983b5a
viniciussanti <vinicius.santi@pucpr.edu.br>
2021-11-09 17:50:37 -0300

    js-branch-index
    Criado Js no index.

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function mudaCadastro() {
+    window.location.href = "paginas/cadastro.html";
+}
+
+function mudaLogin() {
+    window.location.href = "paginas/login.html";
+}
+
+function mudaCarrinho() {
+    window.location.href = "paginas/carrinho.html";
+}
+
+function mudaProdutos() {
+    window.location.href = "paginas/produtos.html";
+}
