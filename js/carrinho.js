fb933de8ae8e12509689569c997a339e61c9b06d
viniciussanti <vinicius.santi@pucpr.edu.br>
2021-11-09 17:29:13 -0300

    js-branch-carrinho
    Criado js + arrumado css e html do carrinho.

---
+++ js/carrinho.js
@@ -0,0 +1,31 @@
+var carrinho = JSON.parse(window.localStorage.getItem("carrinho"));
+
+window.onload = function(){
+    montarCardProdutos();
+}
+
+function montarCardProdutos(){
+
+    document.getElementById("divCarrinho").innerHTML = "";
+
+
+    for(var i = 0; i < carrinho.length; i++)
+    {
+        var conteudo = "";
+        conteudo += '<div class="div-card" >';
+        conteudo += '<div class="div-card-img">';
+        conteudo += '<img src="../img/' + carrinho[i][2] + '" />';
+        conteudo += '</div>';
+        conteudo += '<div class="div-card-descricao">';
+        conteudo += carrinho[i][1];
+        conteudo += '</div>';
+
+        conteudo += '<div class="div-card-comprar div-card-comprado"   >';
+        conteudo += 'Comprado';
+        conteudo += '</div>';
+
+        conteudo += '</div>';
+        
+        document.getElementById("divCarrinho").innerHTML += conteudo;
+    }
+}
