bf0141b1d46fdd62bd5e265619a3be10822a2604
viniciussanti <vinicius.santi@pucpr.edu.br>
2021-11-09 18:10:26 -0300

    js-branch-login
    Criado Js da página de login

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function mudaCarrinho() {
+    window.location.href = "carrinho.html";
+}
+
+function mudaIndex() {
+    window.location.href = "../index.html";
+}
+
+function mudaCadastro() {
+    window.location.href = "cadastro.html";
+}
+
+
